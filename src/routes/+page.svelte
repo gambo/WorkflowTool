65cf2a0b8e8b84c28c0a8dbb5275d5df1be9eb67
Steven Joubert <stevenjoubert@gmail.com>
2025-07-01 09:27:52 +0200

    Hashing out skeleton
    Co-authored-by: Shaun Richardson <srconstruct@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	type Project = Set<Stage>;
+
+	type QuestionType = string | boolean | number;
+	type Question = { question: string; type: QuestionType; required: Boolean };
+	type Stage = {
+		created: Date;
+		name: string;
+		description: string;
+		updated: Date;
+		questions: Question[];
+		id: ID;
+	};
+    
+
+	type Task = {
+		created: Date;
+		updated: Date;
+		name: string;
+		description: string;
+		id: ID;
+		answers: Answers[];
+	};
+
+	type Answers = Array<string | boolean | number>;
+	type ID = string | number;
+
+    type ProgressRequest = {
+    }
+
+	const makJam: Stage = {
+		created: new Date(),
+		description: 'Make some jam yo',
+		id: Math.random(),
+		name: 'makeJam',
+		updated: new Date()
+	};
+
+	const makTin: Stage = {
+		created: new Date(),
+		description: 'Make some tin yo',
+		id: Math.random(),
+		name: 'makeTin',
+		updated: new Date()
+	};
+
+	const jamInTin: Stage = {
+		created: new Date(),
+		description: 'Put some jam in tin yo',
+		id: Math.random(),
+		name: 'Jam in Tin Sir',
+		updated: new Date()
+	};
+
+	const packagetins: Stage = {
+		created: new Date(),
+		description: 'Put some package in my tin yo',
+		id: Math.random(),
+		name: 'package tine',
+		updated: new Date()
+	};
+
+	const mineProject: Project = new Set([packagetins, makJam, makTin, jamInTin]);
+</script>
