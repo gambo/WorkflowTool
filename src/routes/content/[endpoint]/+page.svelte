b2ecf1033fb3d7bec2a6a63554a7d90b2d337883
Shaun Richardson <srconstruct@gmail.com>
2025-09-02 11:23:40 +0000

    Add Dialog Form component Fixes #8

--- src/routes/content/[endpoint]/+page.svelte
+++ src/routes/content/[endpoint]/+page.svelte
@@ -3,6 +3,8 @@
 	import AutoTable from '$lib/Components/AutoTable.svelte';
 	import AutoForm from '$lib/Form/AutoForm.svelte';
 	import type { PageData } from './$types';
+	import Dialog from '$lib/Components/Dialog.svelte';
+	import { add_icon } from '$lib/Icons.svelte';
 
 	type Props = {
 		data: PageData;
@@ -12,9 +14,22 @@
 	let { data }: Props = $props();
 </script>
 
-<div class="mb-8 grid border-b border-b-slate-600 pb-2">
-	<h1 class="text-lg font-medium">{data.label}</h1>
-	<h2 class="text-slate-600">{data.description}</h2>
+<div class="mb-8 flex items-center justify-between border-b border-b-slate-600 pb-2">
+	<div class="grid">
+		<h1 class="text-lg font-medium">{data.label}</h1>
+		<h2 class="text-slate-600">{data.description}</h2>
+	</div>
+	<div>
+		<Dialog>
+			{#snippet button()}
+				<div class="flex items-center gap-1">
+					{@render add_icon()}
+					Add {data.endpoint}
+				</div>
+			{/snippet}
+			<AutoForm schema={data.form} add={data.add} />
+		</Dialog>
+	</div>
 </div>
 <AutoTable
 	{...data}
@@ -23,4 +38,3 @@
 		check: 'boolean'
 	}}
 />
-<AutoForm schema={data.form} add={data.add} />
