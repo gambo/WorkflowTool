8aee741e1b119226b96a49a76645fbb07ec41fcd
Shaun Richardson <srconstruct@gmail.com>
2025-09-02 11:27:55 +0000

    more space

--- src/lib/Form/Field.svelte
+++ src/lib/Form/Field.svelte
@@ -15,61 +15,63 @@
 	let { name, data, required }: Props = $props();
 </script>
 
-{#if data.widget === 'hidden'}
-	<!-- Purposefully blank -->
-{:else if data.format === 'date-time'}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type="date" {required} />
-{:else if data.widget === 'color'}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type="color" {required} />
-{:else if data.widget === 'password'}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type="password" {required} />
-{:else if data.widget === 'email'}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type="email" {required} />
-{:else if data.widget === 'lookup'}
-	<svelte:boundary>
-		{#snippet pending()}
-			loading references
-		{/snippet}
-		{#snippet failed(e)}
-			{#if e instanceof Error}{e.message}
-			{:else}unknown error{/if}
-		{/snippet}
-		{@const title = data.widget?.label ?? titlecase(name)}
-		{@const { key, label } = data}
-		{@const list = await import(`../../lib/db/${data.table}/funcs.remote.ts`).then(x => x.list)}
-		{@const entries = await list()}
-		<Select label={title} {name} {required}>
-			<option>Select an option</option>
-			{#each entries as option}
-				<option value={option[key]}>{option[label]}</option>
+<div class="field mb-3">
+	{#if data.widget === 'hidden'}
+		<!-- Purposefully blank -->
+	{:else if data.format === 'date-time'}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type="date" {required} />
+	{:else if data.widget === 'color'}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type="color" {required} />
+	{:else if data.widget === 'password'}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type="password" {required} />
+	{:else if data.widget === 'email'}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type="email" {required} />
+	{:else if data.widget === 'lookup'}
+		<svelte:boundary>
+			{#snippet pending()}
+				loading references
+			{/snippet}
+			{#snippet failed(e)}
+				{#if e instanceof Error}{e.message}
+				{:else}unknown error{/if}
+			{/snippet}
+			{@const title = data.widget?.label ?? titlecase(name)}
+			{@const { key, label } = data}
+			{@const list = await import(`../../lib/db/${data.table}/funcs.remote.ts`).then(x => x.list)}
+			{@const entries = await list()}
+			<Select label={title} {name} {required}>
+				<option>Select an option</option>
+				{#each entries as option}
+					<option value={option[key]}>{option[label]}</option>
+				{/each}
+			</Select>
+		</svelte:boundary>
+	{:else if data.type === 'string' && !data.enum}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type={data.type} {required} />
+	{:else if data.type === 'number' || data.type === 'integer'}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type={data.type} {required} />
+	{:else if data.type === 'boolean'}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type="checkbox" {required} />
+	{:else if data.type === 'string' && data.format === 'color'}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Input {label} {name} type="color" {required} />
+	{:else if data.enum}
+		{@const label = data.widget?.label ?? titlecase(name)}
+		<Select {label} {name} {required}>
+			{#each data.enum as option}
+				<option value={option}>{option}</option>
 			{/each}
 		</Select>
-	</svelte:boundary>
-{:else if data.type === 'string' && !data.enum}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type={data.type} {required} />
-{:else if data.type === 'number' || data.type === 'integer'}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type={data.type} {required} />
-{:else if data.type === 'boolean'}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type="checkbox" {required} />
-{:else if data.type === 'string' && data.format === 'color'}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Input {label} {name} type="color" {required} />
-{:else if data.enum}
-	{@const label = data.widget?.label ?? titlecase(name)}
-	<Select {label} {name} {required}>
-		{#each data.enum as option}
-			<option value={option}>{option}</option>
-		{/each}
-	</Select>
-{:else if data.anyOf}
-	<Field {name} data={data.anyOf[0]} {required} />
-{:else}
-	<pre>{JSON.stringify({ name, data, required }, null, 2)}</pre>
-{/if}
+	{:else if data.anyOf}
+		<Field {name} data={data.anyOf[0]} {required} />
+	{:else}
+		<pre>{JSON.stringify({ name, data, required }, null, 2)}</pre>
+	{/if}
+</div>
